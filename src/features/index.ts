c17a16311c213781422c78447a92fc51943ea29f
MarkTanashchuk <mark.tanashchuk@gmail.com>
2022-11-21 07:21:04 +0200

    Initial structure

---
+++ src/features/index.ts
@@ -0,0 +1,5 @@
+import { Chat } from "./chat";
+import { Panel, PanelProps } from "./panel";
+
+export { Chat };
+export { Panel, type PanelProps };
---
+++ src/setupTests.ts
@@ -0,0 +1,26 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+
+// "paths": {
+//   "@assets/*": [
+//     "./assets/*"
+//   ],
+//   "@routes/*": [
+//     "./routes/*"
+//   ],
+//   "@features/*": [
+//     "./features/*"
+//   ],
+//   "@components/*": [
+//     "./components/*"
+//   ],
+//   "@stores/*": [
+//     "./stores/*"
+//   ],
+//   "@lib/*": [
+//     "./lib/*"
+//   ]
+// }
